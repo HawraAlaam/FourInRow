bca6b2262575bad16063b24527caeee6d8befd5c
HawraAlaam <Hawraalaam@gmail.com>
2025-09-14 14:34:18 +0300

    update GAME

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("work")
+//VARIABLE IS HERE
+let board = document.querySelector(".board")
+const disc = document.querySelector(".red")
+
+let turn = 0
+
+console.log(board)
+//FUNCTION IS HERE
+
+for (let i = 1; i <= 42; i++) {
+  const cells = document.createElement("div")
+  board.appendChild(cells)
+  cells.classList.add("cells")
+}
+
+let element
+//EVENTLISTENER IS HERE
+for (let i = 0; i < board.length; i++) {
+  element = board[i]
+  console.log(board[i])
+  element.addEventListener("click", () => {})
+}
