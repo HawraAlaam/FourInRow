54e88d4d616df0157759a29489eb752c9c9df620
HawraAlaam <Hawraalaam@gmail.com>
2025-09-15 11:34:01 +0300

    update handleClick

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //VARIABLE IS HERE
 let board = document.querySelector(".board")
-const disc = document.querySelector(".red")
-
 let turn = 0
-
+let playerTurn = "red"
 //FUNCTION IS HERE
 
 for (let i = 1; i <= 42; i++) {
@@ -11,13 +9,21 @@ for (let i = 1; i <= 42; i++) {
   board.appendChild(cells)
   cells.classList.add("cells")
 }
+
 let allCells = board.querySelectorAll(".cells")
+
 const handleClick = (cell) => {
-  cell.innerText = 1
+  if (cell.style.backgroundColor === "") {
+    cell.style.backgroundColor = playerTurn
+    if (playerTurn === "red") {
+      playerTurn = "yellow"
+    } else {
+      playerTurn = "red"
+    }
+  }
 }
 
 //EVENTLISTENER IS HERE
-allCells.forEach((cell)=>{
-  cell.addEventListener("click" , () => handleClick(cell))
+allCells.forEach((cell) => {
+  cell.addEventListener("click", () => handleClick(cell))
 })
-
