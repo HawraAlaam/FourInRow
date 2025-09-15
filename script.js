c343312679da8048244871371d7b729884a26936
HawraAlaam <Hawraalaam@gmail.com>
2025-09-15 19:55:41 +0300

    update handleClick

--- script.js
+++ script.js
@@ -2,25 +2,40 @@
 let board = document.querySelector(".board")
 let turn = 0
 let playerTurn = "red"
-//FUNCTION IS HERE
 
-for (let i = 1; i <= 42; i++) {
-  const cells = document.createElement("div")
-  board.appendChild(cells)
-  cells.classList.add("cells")
+//FUNCTION IS HERE
+for (let rows = 0; rows < 6; rows++) {
+  for (let cols = 0; cols < 7; cols++) {
+    const cells = document.createElement("div")
+    board.appendChild(cells)
+    cells.classList.add("cells")
+  }
 }
 
 let allCells = board.querySelectorAll(".cells")
+allCells.forEach((div, index) => {
+  div.id = index
+})
 
 const handleClick = (cell) => {
-  if (cell.style.backgroundColor === "") {
-    cell.style.backgroundColor = playerTurn
-    if (playerTurn === "red") {
-      playerTurn = "yellow"
-    } else {
-      playerTurn = "red"
+  const index = parseInt(cell.id)
+  const col = index % 7
+
+  for (let row = 5; row >= 0; row--) {
+    const i = row * 7 + col
+    const currentCell = allCells[i]
+
+    if (currentCell.style.backgroundColor === "") {
+      currentCell.style.backgroundColor = playerTurn
+      if (playerTurn === "red"){
+        playerTurn = "yellow"
+      }else{
+        playerTurn = "red"
+      }
+      return currentCell
     }
   }
+  return null
 }
 
 //EVENTLISTENER IS HERE
